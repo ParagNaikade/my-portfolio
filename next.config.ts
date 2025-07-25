3f46dd085047873cf885c8b5910979310d1e9b7a
Parag Naikade <parag.naikade@standards.org.au>
2025-07-25 16:52:08 +1000

    make it ready for cloudfare deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // required for static export
+  images: {
+    unoptimized: true, // Cloudflare Pages doesn’t optimize images by default
+  },
 };
 
 export default nextConfig;
