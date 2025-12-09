e73d1346487ee2de8f6190e6d2be0d52f7218f66
Parag Naikade <parag.naikade@standards.org.au>
2025-12-10 10:43:19 +1100

    fix

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,8 +1,4 @@
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
-
   const { message } = req.body;
 
   console.log("Received message:", req);
